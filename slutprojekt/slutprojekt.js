9c803c3c215775f2184e7737299ed9f42cec8882
hugo <hugoericson@hotmail.com>
2020-06-02 10:43:12 +0200

    slutprojekt

--- slutprojekt/slutprojekt.js
+++ slutprojekt/slutprojekt.js
@@ -16,28 +16,38 @@ elementKnappPlus.addEventListener("click", summera);
 
 function summera() {
     /* Läs av tal1 & 2 */
-    var tal1 = Number(elementRutaTal1.value);
-    var tal2 = Number(elementRutaTal2.value);
+    var tal1 = (elementRutaTal1.value);
+    var tal2 = (elementRutaTal2.value);
 
-    /* Räkna ut tal1 + tal2 */
-    var summa = tal1 + tal2;
+    if (isNaN(tal1) || isNaN(tal2)) {
+        elementRutaResultat.value = "Det går inte att använda bokstäver";
+    } else {
+        /* Räkna ut tal1 + tal2 */
+        var summa = Number(tal1) + Number(tal2);
 
-    /* Skriver ut svaret i resultat rutan */
-    elementRutaResultat.value = summa;
+        /* Skriver ut svaret i resultat rutan */
+        elementRutaResultat.value = summa;
+    }
+
+
 }
 /* Lyssna på om man klickar på - knappen */
 elementKnappMinus.addEventListener("click", subtraktion);
 
 function subtraktion() {
     /* Läs av tal1 & 2 */
-    var tal1 = Number(elementRutaTal1.value);
-    var tal2 = Number(elementRutaTal2.value);
+    var tal1 = (elementRutaTal1.value);
+    var tal2 = (elementRutaTal2.value);
 
-    /* Räkna ut tal1 - tal2 */
-    var summa = tal1 - tal2;
+    if (isNaN(tal1) || isNaN(tal2)) {
+        elementRutaResultat.value = "Det går inte att använda bokstäver";
+    } else {
+        /* Räkna ut tal1 - tal2 */
+        var summa = Number(tal1) - Number(tal2);
 
-    /* Skriver ut svaret i resultat rutan */
-    elementRutaResultat.value = summa;
+        /* Skriver ut svaret i resultat rutan */
+        elementRutaResultat.value = summa;
+    }
 }
 
 /* Lyssna på om man klickar på * knappen */
@@ -45,14 +55,18 @@ elementKnappGånger.addEventListener("click", multiplikation);
 
 function multiplikation() {
     /* Läs av tal1 & 2 */
-    var tal1 = Number(elementRutaTal1.value);
-    var tal2 = Number(elementRutaTal2.value);
+    var tal1 = (elementRutaTal1.value);
+    var tal2 = (elementRutaTal2.value);
 
-    /* Räkna ut tal1 * tal2 */
-    var summa = tal1 * tal2;
+    if (isNaN(tal1) || isNaN(tal2)) {
+        elementRutaResultat.value = "Det går inte att använda bokstäver";
+    } else {
+        /* Räkna ut tal1 * tal2 */
+        var summa = Number(tal1) * Number(tal2);
 
-    /* Skriver ut svaret i resultat rutan */
-    elementRutaResultat.value = summa;
+        /* Skriver ut svaret i resultat rutan */
+        elementRutaResultat.value = summa;
+    }
 }
 
 /* Lyssna på om man klickar på / knappen */
@@ -60,38 +74,59 @@ elementKnappDividera.addEventListener("click", division);
 
 function division() {
     /* Läs av tal1 & 2 */
-    var tal1 = Number(elementRutaTal1.value);
-    var tal2 = Number(elementRutaTal2.value);
+    var tal1 = (elementRutaTal1.value);
+    var tal2 = (elementRutaTal2.value);
+    if (isNaN(tal1) || isNaN(tal2)) {
+        elementRutaResultat.value = "Det går inte att använda bokstäver";
+        
+    } else {
+        /* Om delar med 0 skriv ut Error */
+        if (tal2 == 0) {
+            elementRutaResultat.value = "Det går inte att dela med noll‬";
+        } else {
+            /* Räkna ut tal1 / tal2 */
+            var summa = Number(tal1) / Number(tal2);
 
-    /* Räkna ut tal1 / tal2 */
-    var summa = tal1 / tal2;
+            /* Skriver ut svaret i resultat rutan */
+            elementRutaResultat.value = summa;
+        }
+    }
 
-    /* Skriver ut svaret i resultat rutan */
-    elementRutaResultat.value = summa;
+
+
+
 }
 elementKnappUpphöjt.addEventListener("click", upphöjt);
 
 function upphöjt() {
     /* Läs av tal1 & 2 */
-    var tal1 = Number(elementRutaTal1.value);
-    var tal2 = Number(elementRutaTal2.value);
+    var tal1 = (elementRutaTal1.value);
+    var tal2 = (elementRutaTal2.value);
 
-    /* Räkna ut tal1 / tal2 */
-    var summa = tal1 ** tal2;
+    if (isNaN(tal1) || isNaN(tal2)) {
+        elementRutaResultat.value = "Det går inte att använda bokstäver";
+    } else {
+        /* Räkna ut tal1 ** tal2 */
+        var summa = Number(tal1) ** Number(tal2);
 
-    /* Skriver ut svaret i resultat rutan */
-    elementRutaResultat.value = summa;
+        /* Skriver ut svaret i resultat rutan */
+        elementRutaResultat.value = summa;
+    }
 }
 elementKnappPi.addEventListener("click", pi);
 
 function pi() {
-    /* Läs av tal1 & 2 */
-    var tal1 = Number(elementRutaTal1.value);
-    var tal2 = Number(elementRutaTal2.value);
-
-    /* Räkna ut tal1 / tal2 */
-    var summa = tal1 * tal2 * 3.14;
-
-    /* Skriver ut svaret i resultat rutan */
-    elementRutaResultat.value = summa;
+     /* Läs av tal1 & 2 */
+     var tal1 = (elementRutaTal1.value);
+     var tal2 = (elementRutaTal2.value);
+ 
+     if (isNaN(tal1) || isNaN(tal2)) {
+         elementRutaResultat.value = "Det går inte att använda bokstäver";
+     } else {
+         /* Räkna ut tal1 * tal2 * pi */
+         var summa = Number(tal1) * Number(tal2) * 3.14;
+ 
+         /* Skriver ut svaret i resultat rutan */
+         elementRutaResultat.value = summa;
+     }
 }
